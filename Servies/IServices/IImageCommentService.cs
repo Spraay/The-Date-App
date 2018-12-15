d00e415ad229501eb97e04d13258a39f63b9af3a
Krystian Rytel <kubarytel@vp.pl>
2018-12-15 12:55:50 +0100

    Updated image service

--- Servies/IServices/IImageCommentService.cs
+++ Servies/IServices/IImageCommentService.cs
@@ -1,11 +1,14 @@
 ﻿using Enties;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Services
 {
     public partial interface IImageCommentService
     {
+        ImageComment Get(Guid id);
+        List<ImageComment> GetList(Guid imageID);
         void Create(Guid imageID, Guid userID, string content);
         void Edit(Guid commentID, string content);
         void Delete(Guid commentID);
@@ -16,6 +19,8 @@ namespace Services
     //TASK PARTIAL
     public partial interface IImageCommentService
     {
+        Task<ImageComment> GetAsync(Guid id);
+        Task<List<ImageComment>> GetListAsync(Guid imageID);
         void CreateAsync(Guid imageID, Guid userID, string content);
         void EditAsync(Guid commentID, string content);
         void DeleteAsync(Guid commentID);
--- Servies/Services/ImageCommentService.cs
+++ Servies/Services/ImageCommentService.cs
@@ -17,7 +17,14 @@ namespace Services
         {
             _context = context;
         }
-
+        public ImageComment Get(Guid id)
+        {
+            return _context.ImagesComments.SingleOrDefault(_=>_.ID == id);
+        }
+        public List<ImageComment> GetList(Guid imageID)
+        {
+            return _context.ImagesComments.Where(_ => _.CommentedItemID == imageID).ToList();
+        }
         public void Create(Guid imageID, Guid userID, string content)
         {
             _context.ImagesComments.Add(new ImageComment()
@@ -27,7 +34,42 @@ namespace Services
             });
             _context.SaveChanges();
         }
-        public async Task AddCommentAsync(Guid imageID, Guid userID, string content)
+        public void Delete(Guid commentID)
+        {
+            var comment = _context.ImagesComments.SingleOrDefault(_ => _.ID == commentID);
+;            _context.ImagesComments.Remove(comment);
+            _context.SaveChanges();
+        }
+        public void Edit(Guid commentID, string content)
+        {
+            var comment = _context.ImagesComments.SingleOrDefault(_ => _.ID == commentID);
+            comment.Content = content;
+            _context.ImagesComments.Update(comment);
+            _context.SaveChanges();
+        }
+        public int Count(Guid imageID)
+        {
+            return _context.ImagesComments.Where(_ => _.CommentedItemID == imageID).Count();
+        }
+        public bool IsAny(Guid imageID)
+        {
+            return _context.ImagesComments.Where(_ => _.CommentedItemID == imageID).Any();
+        }
+        
+    }
+
+    //TASK PART
+    public partial class ImageCommentService : IImageCommentService
+    {
+        public async Task<ImageComment> GetAsync(Guid id)
+        {
+            return await _context.ImagesComments.SingleOrDefaultAsync(_ => _.ID == id);
+        }
+        public async Task<List<ImageComment>> GetListAsync(Guid imageID)
+        {
+            return await _context.ImagesComments.Where(_ => _.CommentedItemID == imageID).ToListAsync();
+        }
+        public async Task CreateAsync(Guid imageID, Guid userID, string content)
         {
             await _context.ImagesComments.AddAsync(new ImageComment()
             {
@@ -36,48 +78,13 @@ namespace Services
             });
             await _context.SaveChangesAsync();
         }
-
-        public void Delete(Guid commentID)
-        {
-            var comment = _context.ImagesComments.SingleOrDefault(_ => _.ID == commentID);
-;            _context.ImagesComments.Remove(comment);
-            _context.SaveChanges();
-        }
-
-        public void Edit(Guid commentID, string content)
-        {
-            var comment = _context.ImagesComments.SingleOrDefault(_ => _.ID == commentID);
-            comment.Content = content;
-            _context.ImagesComments.Update(comment);
-            _context.SaveChanges();
-        }
-
-        public int Count(Guid imageID)
-        {
-            return _context.ImagesComments.Where(_ => _.CommentedItemID == imageID).Count();
-        }
-
-        public async Task<int> CountImageCommentsAsync(Guid imageID)
+        public async Task<int> CountAsync(Guid imageID)
         {
             return await _context.ImagesComments.Where(_ => _.CommentedItemID == imageID).CountAsync();
         }
-
-        public bool IsAny(Guid imageID)
-        {
-            return _context.ImagesComments.Where(_ => _.CommentedItemID == imageID).Any();
-        }
-
-        public async Task<bool> IsAnyCommentsAsync(Guid imageID)
+        public async Task<bool> IsAnyAsync(Guid imageID)
         {
             return await _context.ImagesComments.Where(_ => _.CommentedItemID == imageID).AnyAsync();
-        }
-
-        public async Task<ImageComment> GetAsync(Guid id)
-        {
-            return await _context.ImagesComments
-                .Include(i => i.CommentedItem)
-                .Include(i => i.Creator)
-                .FirstOrDefaultAsync(m => m.CreatorID == id);
         }
     }
 }
