044d3b1a024b2aec964a54cf1f41a6c347d7cd55
Krystian Rytel <kubarytel@vp.pl>
2018-12-28 16:59:11 +0100

    Added async tasks interface

--- Enties/Abstract/IEntityBaseRepository.cs
+++ Enties/Abstract/IEntityBaseRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace App.Abstract
 {
-    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    public partial interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
         IEnumerable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties);
         IEnumerable<T> GetAll();
@@ -19,5 +20,21 @@ namespace App.Abstract
         void Delete(T entity);
         void DeleteWhere(Expression<Func<T, bool>> predicate);
         void Commit();
+    }
+
+    public partial interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> AllIncludingAsync(params Expression<Func<T, object>>[] includeProperties);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<int> CountAsync();
+        Task<T> GetSingleAsync(Guid id);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> predicate);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        Task<IEnumerable<T>> FindByAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task DeleteWhereAsync(Expression<Func<T, bool>> predicate);
+        Task CommitAsync();
     }
 }
