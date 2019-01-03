b959edebc68f98ad0f1dfdd08053df6d998bc8f9
Krystian Rytel <kubarytel@vp.pl>
2019-01-03 18:05:01 +0100

    Modified migrations

--- DAO/Migrations/20190103150548_interestskey.Designer.cs
+++ DAO/Migrations/20190103150548_interestskey.Designer.cs
@@ -4,14 +4,16 @@ using App.DAO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace App.DAO.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190103150548_interestskey")]
+    partial class interestskey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
