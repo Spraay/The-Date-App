93dc6e0381e780c9522d770271b5b20215b5c1d4
Krystian Rytel <kubarytel@vp.pl>
2019-01-04 13:24:47 +0100

    Added service and repo to the startup

--- DatingApplicationV2/Startup.cs
+++ DatingApplicationV2/Startup.cs
@@ -45,8 +45,11 @@ namespace DatingApplicationV2
 
             services.AddScoped<IInterestRepository, InterestRepository>();
             services.AddScoped<IInterestUserRepository, InterestUserRepository>();
-            services.AddScoped<IUserRepository, UserRepository>();
 
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            services.AddScoped<IUserRoleService, UserRoleService>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddTransient<IEmailSender, EmailSender>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IImageService, ImageService>();
