ff36859cb1c319256216f7f1ba073c0d2e5e234b
Krystian Rytel <kubarytel@vp.pl>
2019-01-07 09:56:24 +0100

    Added new enties

---
+++ Enties/Entities/RealMeet.cs
@@ -0,0 +1,20 @@
+﻿using App.Model.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Model.Entities
+{
+    public class RealMeet : IEntityBase
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; }
+
+        public User ThePersonWhoMet { get; set; }
+
+        public User ThePersonHeMet { get; set; }
+
+        public Guid ThePersonWhoMetId { get; set; }
+        public Guid ThePersonHeMetId { get; set; }
+    }
+}
---
+++ Enties/Entities/Vote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Model.Entities
+{
+    public class Vote
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; }
+
+        public User ThePersonWhoVote { get; set; }
+
+        public User ThePersonHeVoted { get; set; }
+
+        public Guid ThePersonWhoVoteId { get; set; }
+        public Guid ThePersonHeVotedId { get; set; }
+
+        public int Value { get; set; }
+    }
+}
