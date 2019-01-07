6721ce480c86a01b180473db70b2a57ac3988c9b
Krystian Rytel <kubarytel@vp.pl>
2019-01-07 22:34:17 +0100

    Added new repo

---
+++ App.Repository/Abstract/IVoteRepository.cs
@@ -0,0 +1,8 @@
+﻿using App.Model.Entities;
+
+namespace App.Repository.Abstract
+{
+    public interface IVoteRepository : IEntityBaseRepository<Vote>
+    {
+    }
+}
---
+++ App.Repository/VoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using App.DAO;
+using App.DAO.Data;
+using App.Model.Entities;
+using App.Repository.Abstract;
+
+namespace App.Repository
+{
+    public class VoteRepository : EntityBaseRepository<Vote>, IVoteRepository
+    {
+        public VoteRepository(ApplicationDbContext context) : base(context) { }
+
+       
+    }
+}
