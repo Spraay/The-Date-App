db99ea37e86b7073896e9211b4cc82ed1bc7aade
Krystian Rytel <kubarytel@vp.pl>
2019-01-09 19:18:05 +0100

    Added repo for images

---
+++ App.Repository/Abstract/IImageRepository.cs
@@ -0,0 +1,17 @@
+﻿using App.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repository.Abstract
+{
+    public interface IImageRepository : IEntityBaseRepository<Image>
+    {
+        bool IsOwner(Guid id, Guid imgId);
+
+        Task<bool> IsOwnerAsync(Guid id, Guid imgId);
+        bool IsExists(Guid id);
+        Task<bool> IsExistsAsync(Guid id);
+    }
+}
