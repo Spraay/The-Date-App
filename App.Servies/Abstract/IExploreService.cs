14eeb7829302f27285998051b31048f47ec53456
Krystian Rytel <kubarytel@vp.pl>
2019-01-11 13:22:39 +0100

    added service for explore users, it's prototype

---
+++ App.Servies/Abstract/IExploreService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using App.Model.Entities;
+using App.Model.View;
+using AutoMapper;
+using Pagination;
+
+namespace App.Service.Abstract
+{
+    public interface IExploreService
+    {
+        Task<IEnumerable<User>> GetOnlyFilledProfilesAsync();
+        Task<PaginatedList<ApplicationUserViewModel>> SortUsers(string currentFilter, string searchString, int? pageSize, int? page, bool sortDesc, params Expression<Func<User, object>>[] sortParams);
+    }
+}
