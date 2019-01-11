40653d89f02aaaf463b34bfa678181590d05bfd4
Krystian Rytel <kubarytel@vp.pl>
2019-01-11 13:24:07 +0100

    settings changed

--- App.TheDate/Controllers/ImageController.cs
+++ App.TheDate/Controllers/ImageController.cs
@@ -36,11 +36,16 @@ namespace DatingApplicationV2.Controllers
 
         public async Task<IActionResult> UserImages(Guid id)
         {
-            var userGallery = await _imageRepository.AllIncludingAsync(_ => _.Likes, _=>_.Comments);
+            var query = _imageRepository
+                .FindByAsync(
+                    _=>_.UserId==id,
+                    _ => _.Likes,
+                    _=>_.Comments,
+                    _=>_.User
+                );
+            ViewBag.User = await _userRepository.GetSingleAsync(id);
             ViewBag.CurrentUserId = _userService.CurrentUserId;
-            var user = await _userRepository.GetSingleAsync(id);
-            ViewBag.GalleryUserName = user.UserName;
-            return View(userGallery.Where(_=>_.UserId==id));
+            return View(await query);
         }
 
         public ActionResult Create()
