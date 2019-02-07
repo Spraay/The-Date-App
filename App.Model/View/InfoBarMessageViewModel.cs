5284b8f8317143363a9ead213c089079a67672ee
Krystian Rytel <kubarytel@vp.pl>
2019-02-07 17:17:07 +0100

    Views update

---
+++ App.Model/View/InfoBarMessageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Model.View
+{
+    public class InfoBarMessageViewModel
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ App.Model/View/SubHeaderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Model.View
+{
+    public class SubHeaderViewModel
+    {
+        public string Title { get; set; } = "some title";
+        public string TitleColour { get; set; } = "white-text";
+        public string TitleIcon { get; set; } = "white-text";
+        public string TitleIconColor { get; set; } = "white";
+        public string Message { get; set; } = "";
+        public string MessageColor { get; set; } = "";
+    }
+}
