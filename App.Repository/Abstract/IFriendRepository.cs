6b3c2890258f86e8c9ac5546167a3d133577e81d
Krystian Rytel <kubarytel@vp.pl>
2019-02-08 11:04:43 +0100

    Added friend repository

---
+++ App.Repository/Abstract/IFriendRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using App.Model.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace App.Repository.Abstract
+{
+    public interface IFriendRepository : IEntityBaseRepository<Friendship>
+    {
+        
+    }
+}
---
+++ App.Repository/FriendRepository.cs
@@ -0,0 +1,11 @@
+﻿using App.DAO.Data;
+using App.Model.Entities;
+using App.Repository.Abstract;
+
+namespace App.Repository
+{
+    public class FriendRepository : EntityBaseRepository<Friendship>, IFriendRepository
+    {
+        public FriendRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
