efbe820a85c75cea1dcf65e5b040c807705db087
Krystian Rytel <kubarytel@vp.pl>
2019-02-08 11:25:31 +0100

    Created basic service, and improved ConversationService class

---
+++ App.Servies/BasicService.cs
@@ -0,0 +1,71 @@
+﻿using App.DAO.Data;
+using App.Model.Assigned;
+using App.Model.Entities;
+using App.Model.Entities.Abstract;
+using App.Repository;
+using App.Repository.Abstract;
+using App.Service.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Service
+{
+    public class BasicService<EntityType> : IBasicService<EntityType> where EntityType : class, IEntityBase, new()
+    {
+        private readonly IEntityBaseRepository<EntityType> _repository;
+
+        public BasicService(IEntityBaseRepository<EntityType> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<EntityType> GetAsync(Guid id)
+        {
+            return await GetAsync(id, null);
+        }
+
+        public async Task<EntityType> GetAsync(Guid id, params Expression<Func<EntityType, object>>[] includeProperties)
+        {
+            return await _repository.GetSingleAsync(_=>_.Id == id, includeProperties);
+        }
+
+        public async Task AddAsync(EntityType entity)
+        {
+            await _repository.AddAsync(entity);
+            await _repository.CommitAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            _repository.DeleteWhere(_ => _.Id == id);
+            await _repository.CommitAsync();
+        }
+
+        public async Task UpdateAsync(EntityType entity)
+        {
+            _repository.Update(entity);
+            await _repository.CommitAsync();
+        }
+
+        public async Task<IEnumerable<EntityType>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task <IEnumerable<EntityType>> FindByAsync(Expression<Func<EntityType, bool>> predicate)
+        {
+            return await FindByAsync(predicate, null);
+        }
+
+        public async Task <IEnumerable<EntityType>> FindByAsync(Expression<Func<EntityType, bool>> predicate, params Expression<Func<EntityType, object>>[] includeProperties)
+        {
+            return await _repository.FindByAsync(predicate, includeProperties);
+        }
+    }
+}
--- App.Servies/ConversationsService.cs
+++ App.Servies/ConversationsService.cs
@@ -1,6 +1,7 @@
 ﻿using App.DAO.Data;
 using App.Model.Assigned;
 using App.Model.Entities;
+using App.Repository;
 using App.Repository.Abstract;
 using App.Service.Abstract;
 using Microsoft.EntityFrameworkCore;
@@ -14,82 +15,35 @@ using System.Threading.Tasks;
 
 namespace App.Service
 {
-    public class ConversationsService : IConversationsService
+    public class ConversationsService : BasicService<Conversation>, IConversationsService 
     {
-        private readonly IConversationRepository _repository;
-        private readonly IFriendRepository _friendRepository;
-        public ConversationsService(IConversationRepository repository)
+        public ConversationsService(IEntityBaseRepository<Conversation> repository) : base(repository)
         {
-            _repository = repository;
+            
         }
 
         public async Task UpdateUsersAsync(Guid id, Guid[] userList)
         {
-            var conversation = _repository.GetSingle(_=>_.Id == id, _=>_.Users);
+            var conversation = await GetAsync(id, _=>_.Users);
             var newUsers = new List<ConversationUser>() { };
             foreach(var userId in userList)
             {
                 newUsers.Add(new ConversationUser() { UserId = userId, ConversationId = id });
             }
             conversation.Users = newUsers;
-            _repository.Update(conversation);
-            await _repository.CommitAsync();
-        }
-
-        public async Task<IEnumerable<Conversation>> GetUserConversationsAsync(Guid currentUserId)
-        {
-            return await _repository.FindByAsync(_ => _.Users.Select(__ => __.UserId).Contains(currentUserId));
-        }
-
-        public async Task<Conversation> GetAsync(Guid id)
-        {
-            return await GetAsync(id, null);
-        }
-
-        public async Task<Conversation> GetAsync(Guid id, params Expression<Func<Conversation, object>>[] includeProperties)
-        {
-            return await _repository.GetSingleAsync(_=>_.Id == id, includeProperties);
-        }
-
-        public async Task AddAsync(Conversation entity)
-        {
-            await _repository.AddAsync(entity);
-            await _repository.CommitAsync();
-        }
-
-        public async Task DeleteAsync(Guid id)
-        {
-            _repository.DeleteWhere(_ => _.Id == id);
-            await _repository.CommitAsync();
-        }
-
-        public async Task UpdateAsync(Conversation entity)
-        {
-            _repository.Update(entity);
-            await _repository.CommitAsync();
-        }
-
-        public async Task<IEnumerable<Conversation>> GetAllAsync()
-        {
-            return await _repository.GetAllAsync();
-        }
-
-        public async Task <IEnumerable<Conversation>> FindByAsync(Expression<Func<Conversation, bool>> predicate)
-        {
-            return await FindByAsync(predicate, null);
-        }
-
-        public async Task <IEnumerable<Conversation>> FindByAsync(Expression<Func<Conversation, bool>> predicate, params Expression<Func<Conversation, object>>[] includeProperties)
-        {
-            return await _repository.FindByAsync(predicate, includeProperties);
+            await UpdateAsync(conversation);
         }
 
         public async Task UpdateNameAsync(Guid id, string name)
         {
             var entity = await GetAsync(id);
             entity.Name = name;
-            _repository.Update(entity);
-            await _repository.CommitAsync();
+            await UpdateAsync(entity);
+        }
+
+        public async Task<IEnumerable<Conversation>> GetUserConversationsAsync(Guid userId)
+        {
+            return await FindByAsync(_ => _.Users.Select(__ => __.UserId).Contains(userId));
         }
     }
 }
