17a7367545d2df406233d4112cef93c9a01c4fae
Krystian Rytel <kubarytel@vp.pl>
2019-02-11 13:38:05 +0100

    Added new models for passing some data to users grid

---
+++ App.Model/View/UserCardLinksViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Model.View
+{
+    public class UserCardLinkViewModel
+    {
+        public string ActionName { get; set; }
+        public string Controller { get; set; }
+        public string LinkText { get; set; }
+        public object RouteValues { get; set; }
+        public object LinkHtmlAttributes { get; set; }
+        public string IconText { get; set; }
+        public object IconHtmlAttributes { get; set; }
+    }
+}
---
+++ App.Model/View/UserCardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using App.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Model.View
+{
+    public class UserCardViewModel
+    {
+        public User User { get; set; }
+        public List<UserCardLinkViewModel> Links { get; set; }
+    }
+}
