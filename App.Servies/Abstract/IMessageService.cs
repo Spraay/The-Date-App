6af839c366be21215520f90ac8b54b9e7390ffe5
Krystian Rytel <kubarytel@vp.pl>
2019-02-13 09:36:04 +0100

    Added message service ( in the future this will remove the conversation service )

---
+++ App.Servies/Abstract/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿using App.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace App.Service.Abstract
+{
+    public interface IMessageService : IEntityBaseService<Message>
+    {
+       
+    }
+}
---
+++ App.Servies/MessageService.cs
@@ -0,0 +1,19 @@
+﻿using App.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Service
+{
+    public class MessageService : EntityBaseService<MessageService>, IMessageService
+    {
+        private readonly IMessageRepository _messageRepository;
+        private readonly IConversationRepository _conversationRepository;
+
+        public MessageService(IMessageRepository messageRepository, IConversationRepository conversationRepository)
+        {
+            _messageRepository = messageRepository;
+            _conversationRepository = conversationRepository;
+        }
+    }
+}
