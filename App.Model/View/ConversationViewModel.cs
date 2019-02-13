9274ff75ca152cc62b5c92c761e34ea7bad6c86e
Krystian Rytel <kubarytel@vp.pl>
2019-02-13 14:25:25 +0100

    Added view model

---
+++ App.Model/View/ConversationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using App.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Model.View
+{
+    public class ConversationViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
