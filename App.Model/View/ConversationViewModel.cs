7f374d653228536b93bdb6739dfb8a22a2e2f61b
Krystian Rytel <kubarytel@vp.pl>
2019-02-13 17:51:20 +0100

    Removed some lines, added data annotations.

--- App.Model/View/ConversationViewModel.cs
+++ App.Model/View/ConversationViewModel.cs
@@ -1,15 +1,19 @@
 ﻿using App.Model.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace App.Model.View
 {
     public class ConversationViewModel
     {
+        [Required]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public List<User> Users { get; set; }
-        public List<Message> Messages { get; set; }
+        [Required]
+        public List<Guid> UserIds { get; set; }
+        public List<MessageViewModel> Messages { get; set; }
     }
 }
--- App.Model/View/MessageViewModel.cs
+++ App.Model/View/MessageViewModel.cs
@@ -1,47 +1,21 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using App.Model.Entities;
 
-namespace Validators
+namespace App.Model.View
 {
     public class MessageViewModel
     {
         [Required]
-        public Guid ID { get; set; }
+        public Guid Id { get; set; }
+
         [Required]
-        [Display(Name = "Author ID")]
-        public string AuthorID { get; set; }
-        [Required]
-        [Display(Name = "Recipient ID")]
-        public string RecipientID { get; set; }
-        [Required]
-        [MinLength(1), MaxLength(256)]
-        [Display(Name = "Content")]
         public string Content { get; set; }
-        [Required]
-        [Display(Name = "Created Date")]
-        public DateTime CreatedDate { get; } = DateTime.UtcNow;
-        [Required]
-        [Display(Name = "Author")]
-        public User Author { get; set; }
-        [Required]
-        [Display(Name = "Recipient")]
-        public User Recipient { get; set; }
 
-        //public class Message_To_MessageViewModel : MapperConfigurationExpression
-        //{
-        //    public Message_To_MessageViewModel()
-        //    {
-        //        CreateMap<Message, MessageViewModel>();
-        //    }
-        //}
+        [Required]
+        public Guid CreatorId { get; set; }
 
-        //public class MessageViewModel_To_Message : MapperConfigurationExpression
-        //{
-        //    public MessageViewModel_To_Message()
-        //    {
-        //        CreateMap<MessageViewModel, Message>();
-        //    }
-        //}
+        [Required]
+        public Guid ConversationId { get; set; }
+
     }
 }
