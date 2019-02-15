f77c027928f704cb20b04648a5be3ad998ffd854
Krystian Rytel <kubarytel@vp.pl>
2019-02-15 19:18:48 +0100

    Added interfaces for api and js api repository

---
+++ App.Model/API/IConversationInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Model.API
+{
+    interface IConversationInfo
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        List<IConversationUser> Users { get; set; }
+    }
+}
---
+++ App.Model/API/IConversationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App.Model.API
+{
+    public interface IConversationUser
+    {
+        Guid Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string ProfileImgSrc { get; set; }
+    }
+}
