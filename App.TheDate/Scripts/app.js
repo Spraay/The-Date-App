4250abff8f2d4a6da96ffa2c7c45c3780af5ccb3
Krystian Rytel <kubarytel@gmail.com>
2019-02-20 13:59:35 +0100

    Removed type scripts and configuration

--- App.TheDate/Scripts/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ConversationApiService_1 = require("./ConversationApiService");
-var service = new ConversationApiService_1.ConversationApiService();
-service.getConversationInfo('5c4fa0c8-6fae-42b4-882f-a89c883025ac');
-//# sourceMappingURL=app.js.map
--- App.TheDate/wwwroot/scripts/index.ts
+++
@@ -1,6 +0,0 @@
-import {ConversationApiService} from "./ConversationApiService";
-
-let service = new ConversationApiService();
-service.getConversationInfo('5c4fa0c8-6fae-42b4-882f-a89c883025ac');
-
-
