3b759d36ec9c38affb43cca22c3508099d9fe73c
Krystian Rytel <kubarytel@gmail.com>
2019-02-22 12:27:25 +0100

    Added node dependencies and gulp.js

---
+++ App.TheDate/gulpfile.js
@@ -0,0 +1,42 @@
+﻿var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    del = require('del');
+
+function loadMaterializeCss() {
+    gulp.src([
+        'node_modules/materialize-css/dist/css/materialize.css'
+    ])
+        .pipe(minifyCSS())
+        .pipe(concat('materialize.css'))
+        .pipe(gulp.dest('wwwroot/css'));
+}
+
+function loadMaterializeJs() {
+    gulp.src([
+        'node_modules/materialize-css/dist/js/materialize.js'
+    ])
+        .pipe(uglify())
+        .pipe(concat('materialize.js'))
+        .pipe(gulp.dest('wwwroot/js'));
+}
+
+function loadJquery() {
+    gulp.src([
+        'node_modules/jquery/dist/jquery.js'
+    ])
+        .pipe(uglify())
+        .pipe(concat('jquery.js'))
+        .pipe(gulp.dest('wwwroot/js'));
+}
+
+
+gulp.task('default', function (done) {
+    console.log("Started default task");
+    loadJquery();
+    loadMaterializeCss();
+    loadMaterializeJs();
+    done();
+});
+
